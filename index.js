e813f42bdf5c84ae116a134fee51437762ab53e1
sunovivid <suno.vivid@gmail.com>
2020-03-06 00:07:34 +0900

    commit

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 5000
+const bodyParser = require('body-parser')
+const { User } = require('./models/User')
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
 
@@ -11,4 +19,17 @@ mongoose.connect('mongodb+srv://sunovivid:HkWcL1tASWMuDG5k@hellocluster-frjs4.mo
 
 
 app.get('/', (req, res) => res.send('Hello World NodeJS!'))
+
+app.post('/register', (req, res) => {
+    //회원가입에 필요한 정보를 client에서 가져와 DB에 넣어줌
+    const user = new User(req.body)
+    user.save((err, userInfo) => {
+        if (err) return res.json({ success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
